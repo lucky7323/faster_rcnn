0ca8e64f749afd1d494ad5701c8e4e6cc6b41ef4
eunho <lucky7323@postech.ac.kr>
2017-11-15 20:45:49 +0900

    sampling training datasets (only use 20samples for each vid)

---
+++ imageSampling.py
@@ -0,0 +1,53 @@
+import numpy as np
+import os
+
+path = './datasets/ILSVRC2015/Data/VID/train/'
+folders = ['a/', 'b/', 'c/']
+
+for i in range(3):
+	fold = folders[i]
+	print('Start to remove files in (' + fold + ') ')
+	for root1, dirs1, files1 in os.walk(path + fold):
+		for idx, dir in enumerate(sorted(dirs1)):
+			if idx%100==0:
+				print(str(idx) + ' iter - Start to remove files in (' + fold + dir + ')')
+			for root, dirs, files in os.walk(path + fold + dir +'/'):
+				for file in files:
+					fnum = file.split('.')
+					if int(fnum[0]) > 20:
+						os.remove(path + fold + dir + '/' + file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
